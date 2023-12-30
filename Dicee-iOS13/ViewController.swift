43ae9b85273c34581eef2f7bbd9f48c2c7413cba
ting1065 <fengtingtang1065@gmail.com>
2023-12-29 18:09:56 -0800

    change dice image once app launched

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        diceImageView1.alpha = 0.5
     }
-
-
 }
 
